c9e5f1bd2d29afa8a0f1cde2709c91a32a1b8d49
htt12 <harrisontruong2@gmail.com>
2018-03-29 14:38:07 -0700

    Completed add item functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,34 @@
 import 'materialize-css/dist/css/materialize.min.css'
-import React from 'react';
-import List from './list'
+import React, {Component} from 'react';
+import List from './list';
+import AddForm from './add_form'
+import Data from '../data/todo_items';
 
-const App = () => (
-    <div>
-        <div className="container center'">
-            
-            <h1 className="center purple lighten-4">TO DO LIST</h1>
-            <List/>
-        </div>
-    </div>
-);
+class App extends Component{
+    constructor(props){
+        super(props)
+
+        this.state = {
+            list: Data,
+        }
+    }
+
+    addItem(item){
+        this.setState({
+            list: [item, ...this.state.list]
+        })
+    }
+    render(){
+        return (
+            <div>
+                <div className="container center">
+                    <h1 className="center purple lighten-4">To Do List</h1>
+                    <List list={this.state.list}/>
+                    <AddForm add={this.addItem.bind(this)}/>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default App;
